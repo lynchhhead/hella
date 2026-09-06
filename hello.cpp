4bedaa21b2df079e503fdc5553de3ab72407980f
simp <sanyagaviuk@gmail.com>
2026-09-07 00:23:21 +0200

    caps

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include <ctime>
-
 int main() {
-    time_t now = time(0);
-    std::cout << "Hello, World! Current time: " << ctime(&now);
+    std::cout << "HELLO, WORLD!" << std::endl;
     return 0;
 }
